847898b7a051efe0aedaf28369c81d3cb4f0aa86
tran van phat <tranphat701@gmail.com>
2023-09-22 09:07:36 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     }
   },
   filename: (req, file, cb) => {
-    cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+    cb(null, `${file.fieldname}_${file.originalname}`);
   },
 });
 const upload = multer({ storage }).single("image");
