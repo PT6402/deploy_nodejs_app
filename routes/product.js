63d4fcb853fb38ec83dfbbc189ce09322dda71a4
tran van phat <tranphat701@gmail.com>
2023-09-22 08:52:07 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const ProductController = require("../controllers/ProductController");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads");
+    cb(null, "/publish/uploads");
   },
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
